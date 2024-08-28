ad7fbe001272ae6bf9b44f44663d2dfb14270dec
Diego Chan <diego.chan@utp.ac.pa>
2024-08-28 15:47:24 -0500

    Adding responsiveness to components
    - Adding responsiveness to Header.svelte
    - Adding responsiveness to /contact/+page.svelte

--- client/src/lib/Header.svelte
+++ client/src/lib/Header.svelte
@@ -2,12 +2,14 @@
     import { onMount } from "svelte";
 
     function loggedIn(): boolean { return false; }
+    function displayNavbar() {
+        const nav = document.getElementById("navigation-wrapper") as HTMLDivElement;
+        nav.style.display = window.innerWidth <= 1024 ? "none" : "flex";
+    }
 
     onMount(()=>{
-        window.addEventListener("resize", ()=>{
-            const nav = document.getElementById("navigation-wrapper") as HTMLDivElement;
-            //   v 
-        });
+        displayNavbar();
+        window.addEventListener("resize", displayNavbar);
     });
 </script>
 <header id="website-header" class="bg-white dark:bg-black text-black dark:text-white flex justify-between items-center px-12 py-4 sticky top-0 min-h-20 border-b border-b-neutral-400 dark:border-b-white">
@@ -15,7 +17,7 @@
         <a href="/" id="index-anchor" class="text-2xl flex items-end gap-6 font-semibold">
             <i class="bi bi-tools"></i>
             <h1 id="page-title">
-                <span>TypeCrafters</span>
+                <span class="hidden md:inline">TypeCrafters</span>
                 <span class="text-red-600">Studio</span>
             </h1>
         </a>
@@ -35,13 +37,13 @@
             <div id="user-options" class="w-full lg:w-auto">
                 {#if !loggedIn()}
                 <div id="user-not-verified" class="flex flex-col lg:flex-row gap-y-4 gap-x-6 text-white">
-                    <a href="/users/signup" class="px-3 py-1 rounded font-semibold bg-red-600 hover:bg-red-700 active:bg-red-800 duration-150 flex justify-center items-center">Sign up</a>
-                    <a href="/users/login" class="px-3 py-1 rounded font-semibold bg-neutral-400 dark:bg-neutral-600 hover:bg-neutral-500 dark:hover:bg-neutral-700 active:bg-neutral-600 dark:active:bg-neutral-800  duration-150 flex justify-center items-center">Log in</a>
+                    <a href="/users/signup" class="px-3 py-1 btn btn-red">Sign up</a>
+                    <a href="/users/login" class="px-3 py-1 btn btn-neutral">Log in</a>
                 </div>
                 {:else}
                 <div id="user-verified" class="flex justify-center">
                     <div id="user-pfp-container" class="w-12 aspect-square h-auto rounded-full flex items-center overflow-hidden">
-                        <img src="https://via.placeholder.com/60" alt="User's profile" class="w-full aspect-square h-auto">
+                        <img src="https://via.placeholder.com/60" alt="User's profile" class="w-full h-full">
                     </div>
                 </div>
                 {/if}
