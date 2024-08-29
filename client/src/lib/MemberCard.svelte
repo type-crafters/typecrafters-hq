7f6189c09b56e7553a39682370e771e7b0563685
Diego Chan <diego.chan@utp.ac.pa>
2024-08-29 16:02:13 -0500

    Added MemberCard.svelte
    - Added member card component

---
+++ client/src/lib/MemberCard.svelte
@@ -0,0 +1,24 @@
+<div class="member-card nicely-shaded">
+    <div class="profile-circle">
+        <img class="profile-picture" src="media/img/member/risa_incontenible.jfif" alt="Diego Chan">
+    </div>
+    <h2 class="member-name">Diego Chan</h2>
+    <h4 class="member-role text-secondary">Chief of Technology</h4>
+</div>
+<style lang="postcss">
+    .member-card {
+        @apply bg-neutral-100 dark:bg-neutral-800 rounded-lg p-4 w-48 flex flex-col items-center gap-4;
+    }
+    .member-card .profile-circle {
+        @apply w-32 aspect-square h-fit rounded-full bg-neutral-100 flex justify-center items-center overflow-hidden;
+    }
+
+    .member-card .profile-circle .profile-picture {
+        @apply w-full h-full;
+    }
+
+    .member-card .member-name {
+        @apply text-xl font-semibold text-center;
+    }
+
+</style>
--- client/src/routes/contact/+page.svelte
+++ client/src/routes/contact/+page.svelte
@@ -1,17 +1,19 @@
 <script lang="ts">
     function copy(id: string): void {
         const span = document.getElementById(id) as HTMLSpanElement;
-        const value = span.innerText /* span.getAttribute("data-value")  */;
+        const value = span.innerText; /* span.getAttribute("data-value")  */
     }
 </script>
+
 <main id="contact-us-page" class="flex flex-col lg:flex-row my-8 md:mx-12 gap-20">
     <section id="email-form" class="md:flex-3">
         <div class="w-4/5 md:w-2/3 2xl:w-1/2 mx-auto">
             <h2 class="text-center text-3xl font-semibold mb-6">Send us an Email</h2>
             <p class="text-center mb-6 mx-2">
-                We're always excited to hear from you. Whether you have a question about one of our games, or feedback you'd
-                like to share with us, don't hesitate to get in touch. Your thoughts and insights help us create better games
-                and experiences.
+                We're always excited to hear from you. Whether you have a question
+                about one of our games, or feedback you'd like to share with us, 
+                don't hesitate to get in touch. Your thoughts and insights help us 
+                create better games and experiences.
             </p>
             <form method="post" class="grid gap-8">
                 <div class="input-wrapper">
@@ -27,7 +29,7 @@
                     <textarea name="message" id="message" class="textbox resize-y" rows="5"></textarea>
                 </div>
                 <div class="input-wrapper">
-                    <input type="submit" value="Send email" class="submit btn-red"/>
+                    <input type="submit" value="Send email" class="submit btn-red" />
                 </div>
             </form>
         </div>
@@ -71,7 +73,14 @@
             <div class="info-row">
                 <div class="info-container">
                     <div id="map-wrapper" class="rounded overflow-hidden w-full md:w-3/5 lg:w-full aspect-[4/3] h-auto my-2">
-                        <iframe class="w-full h-full" title="Map to our HQ" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1970.2219670433838!2d-79.53304449340597!3d9.023204796768331!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8faca9d8f2e76d39%3A0xaaaa4bc649942528!2sUniversidad%20Tecnol%C3%B3gica%20de%20Panam%C3%A1%20-%20Edificio%203.!5e0!3m2!1sen!2spa!4v1724778236433!5m2!1sen!2spa" allowfullscreen loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                        <iframe
+                            class="w-full h-full"
+                            title="Map to our HQ"
+                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1970.2219670433838!2d-79.53304449340597!3d9.023204796768331!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8faca9d8f2e76d39%3A0xaaaa4bc649942528!2sUniversidad%20Tecnol%C3%B3gica%20de%20Panam%C3%A1%20-%20Edificio%203.!5e0!3m2!1sen!2spa!4v1724778236433!5m2!1sen!2spa"
+                            allowfullscreen
+                            loading="lazy"
+                            referrerpolicy="no-referrer-when-downgrade"
+                        ></iframe>
                     </div>
                 </div>
             </div>
